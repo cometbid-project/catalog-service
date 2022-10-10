fcd62bf97e2d3a2f0929c60d5bfc3ce76d9cbda1
cometbid-project <20684020+cometbid-project@users.noreply.github.com>
2022-10-10 03:35:48 -0400

    my first commit
    A cloud native project on book catalog service first commit

---
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,18 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Gbenga
+ *
+ */
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gbenga
+ *
+ */
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return "Welcome to the book catalog!";
+	}
+
+}
